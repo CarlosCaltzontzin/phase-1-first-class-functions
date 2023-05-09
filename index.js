7b9b9b0696c43d23ccd6ad6b12508f52dcf72b4b
Carlos Caltzontzin <cacr71993@yahoo.com>
2023-05-08 23:25:44 -0500

    completed 1 first class functions

---
+++ index.js
@@ -0,0 +1,12 @@
+function receivesAFunction(callback){
+    return callback()
+}
+
+function returnsANamedFunction(){
+    function namedFunction(){}
+    return namedFunction
+}
+function returnsAnAnonymousFunction(){
+    return function(){}
+
+}
